5fa328e651848ea6f9b8f980bc201bdc01d3a198
Vishal Vyamajala <vsv12mar@gmail.com>
2025-07-21 17:08:18 -0400

    Update build.sh for c compiler support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,42 +1,46 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -ex
+
+# Add compilation flags for disabling float16
 export CFLAGS="${CFLAGS} -D__NO_FLOAT16"
 export CPPFLAGS="${CPPFLAGS} -D__NO_FLOAT16"
 
-cd "$SRC_DIR/prody/proteins/hpbmodule"
+# Change into the HPB module directory
+cd "${SRC_DIR}/prody/proteins/hpbmodule"
 
-# Check Fortran compiler
-if [ -z "$FC" ]; then
+# Verify Fortran compiler is set
+if [ -z "${FC}" ]; then
   echo "Fortran compiler (\$FC) not set. Aborting."
   exit 1
 fi
 
-# Compile Fortran code
-$FC -O3 -fPIC -c reg_tet.f
+# Compile the Fortran source
+"${FC}" -O3 -fPIC -c reg_tet.f
 
-# Get Python compile & link info
-PYTHON_INCLUDE=$($PYTHON -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
-PYTHON_LIBDIR=$($PYTHON -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
-PYTHON_LIB=$($PYTHON -c "import sysconfig; print(sysconfig.get_config_var('LDLIBRARY'))")
-PYTHON_LIB_NAME=$(basename "$PYTHON_LIB" | sed -E 's/^lib(.*)\.(so|dylib|a)$/\1/')
+# Gather Python include & library paths
+PYTHON_INCLUDE="$(${PYTHON} -c 'from distutils.sysconfig import get_python_inc; print(get_python_inc())')"
+PYTHON_LIBDIR="$(${PYTHON} -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')"
+PYTHON_LIB="$(${PYTHON} -c 'import sysconfig; print(sysconfig.get_config_var("LDLIBRARY"))')"
+PYTHON_LIB_NAME="$(basename "${PYTHON_LIB}" | sed -E 's/^lib(.*)\.(so|dylib|a)$/\1/')"
 
-# Compile C++ wrapper
-g++ -O3 -g -fPIC -c hpbmodule.cpp -o hpbmodule.o -I${PYTHON_INCLUDE}
+# Compile the C++ wrapper using the conda-provided compiler
+"${CXX}" -O3 -g -fPIC -c hpbmodule.cpp -o hpbmodule.o -I"${PYTHON_INCLUDE}"
 
-# Export library path
-export LIBRARY_PATH=$PREFIX/lib:$PYTHON_LIBDIR:$LIBRARY_PATH
+# Ensure linker can find libraries
+export LIBRARY_PATH="${PREFIX}/lib:${PYTHON_LIBDIR}:${LIBRARY_PATH}"
 
-# Link .so
+# Link into a shared library (.so/.dylib)
 if [[ "$(uname)" == "Darwin" ]]; then
-  g++ -dynamiclib -o hpb.so hpbmodule.o reg_tet.o -L${PYTHON_LIBDIR} -l${PYTHON_LIB_NAME} -lgfortran -undefined dynamic_lookup
+  "${CXX}" -dynamiclib -o hpb.so hpbmodule.o reg_tet.o \
+    -L"${PYTHON_LIBDIR}" -l"${PYTHON_LIB_NAME}" -lgfortran -undefined dynamic_lookup
 else
-  g++ -shared -Wl,-soname,hpb.so -o hpb.so hpbmodule.o reg_tet.o -L${PYTHON_LIBDIR} -l${PYTHON_LIB_NAME} -lgfortran
+  "${CXX}" -shared -Wl,-soname,hpb.so -o hpb.so hpbmodule.o reg_tet.o \
+    -L"${PYTHON_LIBDIR}" -l"${PYTHON_LIB_NAME}" -lgfortran
 fi
 
-# Move shared object
-cp hpb.so ../
+# Move the shared library back into the package directory
+mv hpb.so ../
 
-cd "$SRC_DIR"
-
-# Final installation
-$PYTHON -m pip install . --no-deps --no-build-isolation -vv
+# Return to the source root and install the package via pip
+cd "${SRC_DIR}"
+"${PYTHON}" -m pip install . --no-deps --no-build-isolation -vv
