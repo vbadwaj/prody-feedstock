bab5e584834c0d521dd4eed121e12e927b2f64f7
Vishal Vyamajala <vsv12mar@gmail.com>
2025-08-10 01:46:59 -0400

    arm 64 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -46,57 +46,34 @@
 # "${PYTHON}" -m pip install . --no-deps --no-build-isolation -vv
 #!/usr/bin/env bash
 #!/usr/bin/env bash
-set -Eeuo pipefail
-set -x
+set -ex
+echo "BUILD=${BUILD:-?} HOST=${HOST:-?} target=${target_platform:-?} build=${build_platform:-?}"
 
-echo "BUILD=${BUILD:-?}  HOST=${HOST:-?}"
-echo "BUILD_PREFIX=${BUILD_PREFIX:-?}"
-echo "PREFIX=${PREFIX:-?}"
-echo "target_platform=${target_platform:-?}  build_platform=${build_platform:-?}"
-echo "CC=$CC"
-echo "CXX=$CXX"
-echo "FC(initial)=${FC:-<unset>}"
-
+# Use cross wrapper if present
 if [[ -n "${BUILD_PREFIX:-}" && -n "${HOST:-}" && -x "${BUILD_PREFIX}/bin/${HOST}-gfortran" ]]; then
   export FC="${BUILD_PREFIX}/bin/${HOST}-gfortran"
 fi
-
-echo "FC(using)=$FC"
-command -v "$FC"
-"$FC" --version || true
-file "$(command -v "$FC")" || true
 
 export CFLAGS="${CFLAGS:-} -D__NO_FLOAT16"
 export CPPFLAGS="${CPPFLAGS:-} -D__NO_FLOAT16"
 export CXXFLAGS="${CXXFLAGS:-} -D__NO_FLOAT16"
 
 pushd "${SRC_DIR}/prody/proteins/hpbmodule"
-
 "$FC" -O3 -fPIC -c reg_tet.f
 
 PY_INC="$("$PYTHON" - <<'PY'
-import sysconfig
-print(sysconfig.get_paths()["include"])
+import sysconfig; print(sysconfig.get_paths()["include"])
 PY
 )"
-
 "$CXX" -O3 -g -fPIC -I"$PY_INC" -c hpbmodule.cpp -o hpbmodule.o
 
 if [[ "$(uname)" == "Darwin" ]]; then
   "$CXX" -dynamiclib -o hpb.so hpbmodule.o reg_tet.o \
-        ${LDFLAGS:-} \
-        -lgfortran \
-        -Wl,-rpath,"${PREFIX}/lib" \
-        -undefined dynamic_lookup
-  (command -v otool >/dev/null && otool -L hpb.so) || true
+        -lgfortran -Wl,-rpath,"${PREFIX}/lib" -undefined dynamic_lookup
 else
   "$CXX" -shared -o hpb.so hpbmodule.o reg_tet.o \
-        ${LDFLAGS:-} \
-        -lgfortran \
-        -Wl,-rpath,"${PREFIX}/lib"
-  (command -v readelf >/dev/null && readelf -d hpb.so | grep RPATH || true)
+        -lgfortran -Wl,-rpath,"${PREFIX}/lib"
 fi
-
 mv -v hpb.so ../
 popd
 
